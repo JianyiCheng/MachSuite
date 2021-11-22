7b84770bdcfa6f8a06bae077e0f47b2fe649985e
kumasento <kumasento@users.noreply.github.com>
2021-11-22 18:03:42 +0000

    Fixed some code

--- stencil/stencil2d/stencil.c
+++ stencil/stencil2d/stencil.c
@@ -9,16 +9,18 @@ stencil_label1:
   for (r = 0; r < row_size - 2; r++) {
   stencil_label2:
     for (c = 0; c < col_size - 2; c++) {
-      temp = (TYPE)0;
+      sol[(r * col_size) + c] = (TYPE)0;
+      // temp = (TYPE)0;
     stencil_label3:
       for (k1 = 0; k1 < 3; k1++) {
       stencil_label4:
         for (k2 = 0; k2 < 3; k2++) {
           mul = filter[k1 * 3 + k2] * orig[(r + k1) * col_size + c + k2];
-          temp += mul;
+          sol[(r * col_size) + c] += mul;
+          // temp += mul
         }
       }
-      sol[(r * col_size) + c] = temp;
+      // sol[(r * col_size) + c] = temp;
     }
   }
 }
