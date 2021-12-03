0263a70968f524affdaacdc5e0a408bb21f9d929
kumasento <kumasento@users.noreply.github.com>
2021-12-03 16:35:51 +0000

    Turn GEMM into 2D

--- gemm/ncubed/gemm.c
+++ gemm/ncubed/gemm.c
@@ -1,6 +1,7 @@
 #include "gemm.h"
 
-void gemm(TYPE m1[N], TYPE m2[N], TYPE prod[N]) {
+void gemm(TYPE m1[row_size][row_size], TYPE m2[row_size][col_size],
+          TYPE prod[row_size][col_size]) {
   int i, j, k;
   int k_col, i_col;
   TYPE mult;
@@ -9,15 +10,18 @@ outer:
   for (i = 0; i < row_size; i++) {
   middle:
     for (j = 0; j < col_size; j++) {
-      i_col = i * col_size;
+      // i_col = i * col_size;
       // TYPE sum = 0;
-      prod[i_col + j] = 0;
+      // prod[i_col + j] = 0;
+      prod[i][j] = 0;
     inner:
       for (k = 0; k < row_size; k++) {
-        k_col = k * col_size;
-        mult = m1[i_col + k] * m2[k_col + j];
-        prod[i_col + j] += mult;
+        // k_col = k * col_size;
+        // mult = m1[i_col + k] * m2[k_col + j];
+        mult = m1[i][k] * m2[k][j];
+        // prod[i_col + j] += mult;
         // sum += mult;
+        prod[i][j] += mult;
       }
       // prod[i_col + j]  = sum;
     }
@@ -27,7 +31,9 @@ outer:
 int main() {
 
   srand(9);
-  TYPE m1[N], m2[N], prod[N];
+  // TYPE m1[N], m2[N], prod[N];
+
+  TYPE m1[row_size][row_size], m2[row_size][col_size], prod[row_size][col_size];
   gemm(m1, m2, prod);
 
   return 0;
--- gemm/ncubed/gemm.h
+++ gemm/ncubed/gemm.h
@@ -1,24 +1,26 @@
-//Standard Libraries
+// Standard Libraries
+#include "support.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "support.h"
 
-//Define compute data type
+// Define compute data type
 #define TYPE double
 
-//Specify row/column sizes
+// Specify row/column sizes
 #define row_size 64
 #define col_size 64
-#define N row_size*col_size
+#define N row_size *col_size
 
-//Define the input range to operate over
+// Define the input range to operate over
 #define MIN 0.
 #define MAX 1.0
 
-//Set number of iterations to execute
+// Set number of iterations to execute
 #define MAX_ITERATION 1
 
-void gemm(TYPE m1[N], TYPE m2[N], TYPE prod[N]);
+// void gemm(TYPE m1[N], TYPE m2[N], TYPE prod[N]);
+void gemm(TYPE m1[row_size][row_size], TYPE m2[row_size][col_size],
+          TYPE prod[row_size][col_size]);
 ////////////////////////////////////////////////////////////////////////////////
 // Test harness interface code.
 
